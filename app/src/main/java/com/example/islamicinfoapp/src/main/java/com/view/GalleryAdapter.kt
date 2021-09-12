5c978dd1b6ed3d3b801b2712e5af3942bf631a07
Syed Zakriya <44648243+syed-zakriya@users.noreply.github.com>
2021-09-13 02:31:44 +0530

    initial commit

--- app/src/main/java/com/example/islamicinfoapp/src/main/java/com/view/GalleryAdapter.kt
+++ app/src/main/java/com/example/islamicinfoapp/src/main/java/com/view/GalleryAdapter.kt
@@ -1,41 +1,63 @@
 package com.example.islamicinfoapp.src.main.java.com.view
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.islamicinfoapp.R
-import com.example.islamicinfoapp.src.main.java.com.model.Constants
 import kotlinx.android.synthetic.main.gallery_item.view.*
 
-class GalleryAdapter(val context: Context?, val list:ArrayList<Bitmap>)
-    : RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder>() {
-
-    val TAG = GalleryAdapter::class.simpleName
+class GalleryAdapter(val context: Context?, val images_list: ArrayList<String>)
+    :RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder {
-        Log.d(Constants.PREG_TAG,"$TAG onCreateViewHolder: ")
         return GalleryViewHolder(LayoutInflater.from(context).
         inflate(R.layout.gallery_item,parent,false))
-
     }
 
-    override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
-        Log.d(Constants.PREG_TAG,"$TAG onBindViewHolder: $position")
-        holder.image.setImageBitmap(list.get(position))
+    override fun onBindViewHolder(holder:GalleryViewHolder, position: Int) {
+        if (context != null){
+            Glide.with(context)
+                    .load(images_list.get(position))
+                    .error(R.drawable.ic_wifi_off)
+                    .override(200,200)
+                    .into(holder.image)
+        }
     }
 
     override fun getItemCount(): Int {
-        Log.d(Constants.PREG_TAG,"$TAG getItemCount: ${list.size}")
-        return list.size
+        return images_list.size
     }
 
-    class GalleryViewHolder(view: View):RecyclerView.ViewHolder(view){
+    class GalleryViewHolder(view:View):RecyclerView.ViewHolder(view){
         val image = view.item_image
     }
-
 }
+
+//class GalleryAdapter(val context: Context?, val list:ArrayList<Bitmap>)
+//    : RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder>() {
+//
+//    val TAG = GalleryAdapter::class.simpleName
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder {
+//        Log.d(Constants.PREG_TAG, "$TAG onCreateViewHolder: ")
+//        return GalleryViewHolder(LayoutInflater.from(context).inflate(R.layout.gallery_item, parent, false))
+//
+//    }
+//
+//    override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
+//        Log.d(Constants.PREG_TAG, "$TAG onBindViewHolder: $position")
+//        holder.image.setImageBitmap(list.get(position))
+//    }
+//
+//    override fun getItemCount(): Int {
+//        Log.d(Constants.PREG_TAG, "$TAG getItemCount: ${list.size}")
+//        return list.size
+//    }
+//
+//    class GalleryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        val image = view.item_image
+//    }
+//}
--- app/src/main/java/com/example/islamicinfoapp/src/main/java/com/view/GalleryFragment.kt
+++ app/src/main/java/com/example/islamicinfoapp/src/main/java/com/view/GalleryFragment.kt
@@ -1,74 +1,93 @@
 package com.example.islamicinfoapp.src.main.java.com.view
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.islamicinfoapp.R
-import com.example.islamicinfoapp.src.main.java.com.model.Constants
-import kotlinx.android.synthetic.main.fragment_gallery.*
+import java.io.IOException
+import java.lang.Exception
 
 class GalleryFragment : Fragment() {
-    val MAX_IMAGE_SIZE = 200
+    //val MAX_IMAGE_SIZE = 200
+    var list = ArrayList<String>()
     val TAG = GalleryFragment::class.simpleName
+    lateinit var g_recyclerview:RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_gallery, container, false)
-        gal_recyclerview?.layoutManager = GridLayoutManager(activity,2)
-        val galleryadapter = GalleryAdapter(context,getItemsList())
-        gal_recyclerview?.adapter = galleryadapter
+        g_recyclerview = view.findViewById(R.id.gal_recyclerview)
+        g_recyclerview.layoutManager = GridLayoutManager(context,2)
+        readFileAndFillArrayList()
+        initialiseAdapter()
+//        val galleryadapter = GalleryAdapter(context,getItemsList())
+//        g_recyclerview.adapter = galleryadapter
         return view
     }
 
-    private fun getItemsList(): ArrayList<Bitmap> {
-        val list = ArrayList<Bitmap>()
-        list.add(scaleDown(R.drawable.prayer_1,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.prayer_2,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.prayer_3,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.mecca_1,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.mecca_2,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.mecca_3,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.mecca_4,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.mecca_5,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.madina_1,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.madina_2,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.madina_3,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.madina_4,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.madina_5,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.preg_1,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.preg_2,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.preg_3,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.preg_4,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.preg_5,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.preg_6,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.preg_7,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.eid_1,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.eid_2,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.eid_3,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.eid_4,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.eid_5,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.quote_1,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.quote_2,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.quote_3,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.quote_4,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.quote_5,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.quote_6,MAX_IMAGE_SIZE,true))
-        list.add(scaleDown(R.drawable.quote_7,MAX_IMAGE_SIZE,true))
-        Log.d(Constants.PREG_TAG, "$TAG getItemsList: ${list.size}"  )
-        return list
+    private fun initialiseAdapter() {
+        val galleryAdapter = GalleryAdapter(context,list)
+        g_recyclerview.adapter = galleryAdapter
+
     }
 
-    private fun scaleDown(image: Int, maxImageSize: Int, b: Boolean): Bitmap {
-        val bitmap = BitmapFactory.decodeResource(resources,image)
-        return Bitmap.createScaledBitmap(bitmap,maxImageSize,maxImageSize,b)
+    private fun readFileAndFillArrayList() {
+        try {
+            context?.assets?.open("images_link_address.txt")
+                    ?.bufferedReader()?.forEachLine { list.add(it) }
+        }
+        catch (e:Exception){
+            e.printStackTrace()
+        }
     }
+
+//    private fun getItemsList(): ArrayList<Bitmap> {
+//
+//        val list = ArrayList<Bitmap>()
+//        Log.d(Constants.PREG_TAG, "$TAG getItemsList: ${list.size}"  )
+//        list.add(scaleDown(R.drawable.prayer_1,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.prayer_2,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.prayer_3,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.mecca_1,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.mecca_2,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.mecca_3,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.mecca_4,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.mecca_5,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.madina_1,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.madina_2,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.madina_3,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.madina_4,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.madina_5,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.preg_1,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.preg_2,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.preg_3,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.preg_4,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.preg_5,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.preg_6,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.preg_7,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.eid_1,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.eid_2,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.eid_3,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.eid_4,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.eid_5,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.quote_1,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.quote_2,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.quote_3,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.quote_4,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.quote_5,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.quote_6,MAX_IMAGE_SIZE,true))
+//        list.add(scaleDown(R.drawable.quote_7,MAX_IMAGE_SIZE,true))
+//        Log.d(Constants.PREG_TAG, "$TAG getItemsList: ${list.size}"  )
+//        return list
+//    }
+//
+//    private fun scaleDown(image: Int, maxImageSize: Int, b: Boolean): Bitmap {
+//        val bitmap = BitmapFactory.decodeResource(resources,image)
+//        return Bitmap.createScaledBitmap(bitmap,maxImageSize,maxImageSize,b)
+//    }
 }
